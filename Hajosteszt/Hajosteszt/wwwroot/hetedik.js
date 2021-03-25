11a27f50cc5bad3a5f17707b40eab82ef6a8245f
Gergely Laki <gergely.laki2@stud.uni-corvinus.hu>
2021-03-25 23:55:35 +0100

    7.ora_anyaga

---
+++ Hajosteszt/Hajosteszt/wwwroot/hetedik.js
@@ -0,0 +1,22 @@
+﻿var kérdések;
+function vissza() {
+    if (kérdésszám == 0) {
+        kérdésszám = kérdések.length - 1;
+        letöltés();
+    }
+    else {
+        kérdésszám--;
+        letöltés();
+    }
+}
+
+function elore() {
+    if (kérdésszám == kérdések.length - 1) {
+        kérdésszám = 0;
+        letöltés();
+    }
+    else {
+        kérdésszám++;
+        letöltés();
+    }
+}
