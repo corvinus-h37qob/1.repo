8b1ab7c4988e5885d6ffc12e6a6543abe4d49fce
Gergely Laki <gergely.laki2@stud.uni-corvinus.hu>
2021-04-22 15:31:24 +0200

    8.ora anyaga

--- Hajosteszt/Hajosteszt/wwwroot/hetedik.js
+++ Hajosteszt/Hajosteszt/wwwroot/hetedik.js
@@ -19,4 +19,36 @@ function elore() {
         kérdésszám++;
         letöltés();
     }
+    fetch('/questions/4')
+        .then(response => response.json())
+        .then(data => console.log(data)
+    );
+    function kérdésMegjelenítés(kérdés) {
+        console.log(kérdés);
+        document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+        document.getElementById("válasz1").innerText = kérdés.answer1
+        document.getElementById("válasz2").innerText = kérdés.answer2
+        document.getElementById("válasz3").innerText = kérdés.answer3
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+    };
+    function kérdésBetöltés(id) {
+        fetch(`/questions/${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    console.error(`Hibás válasz: ${response.status}`)
+                }
+                else {
+                    return response.json()
+                }
+            })
+            .then(data => kérdésMegjelenítés(data));
+    }    
+    function válaszfeldolgozás(válasz) {
+        if (!válasz.ok) {
+            console.error(`Hibás válasz: ${response.status}`)
+        }
+        else {
+            return válasz.json()
+        }
+    }
 }
