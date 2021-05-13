ad94176f3ae569b0442ddb8d5883ed3daa3d65ae
Gergely Laki <gergely.laki2@stud.uni-corvinus.hu>
2021-05-13 10:49:35 +0200

    11.orai

---
+++ Hajosteszt/Hajosteszt/Controllers/QuestionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hajosteszt.Controllers
+{
+   // [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        [HttpGet]
+        [Route("question/count")]
+        public int M1()
+        {
+            HajostesztContext context = new HajostesztContext();
+            int kérdésekSzáma = context.Questions.Count();
+            return kérdésekSzáma;
+        }
+        [HttpGet]
+        [Route("question/sorszam")]
+
+        public ActionResult M2(int sorszam)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var krdés = (from x in context.Questions
+                         where x.QuestionId == sorszám
+                         select x).FirstOrDefault();
+            if (kérdés == null) return BadRequest("Nincs ilyen kérdées");
+            return new JsonResult(kérdés);
+        }
+    }
+}
---
+++ Hajosteszt/Hajosteszt/wwwroot/11_ora.js
@@ -0,0 +1,37 @@
+﻿var hotList = [];
+var questionsInHotList = 3;
+var displayedQuestion;
+var numberofQuestions;
+var nextQuestion = 1;
+
+function init() {
+    for (let i = 0; i < QusteionsInHotList; i++) {
+        hotlist[i] = {
+            question: {},
+            goodAnswers: 0
+        }
+    }
+
+    for (let i = 0; i < questionsInHotList; i++) {
+        kérdésbetöltés(nextQuestion, i);
+        nextQuestion++;
+
+    }
+
+}
+
+function kérdésbetöltés(questionnumber, destination) {
+    fetch('/questions/${questionumber}')
+        .then(result => {
+            if (!result.ok) {
+                conolse.error('hibás letöltés: ${result.status}');
+                return null;
+            }
+            else {
+                return result.json();
+            }
+        })
+        .then(q => {
+            hotlist[destination] = q;
+        })
+}
